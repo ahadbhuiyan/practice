3506f133ea946962c47a62fa459c941ed8b81145
unknown <bhuiyan.abdullah.cs@gmail.com>
2019-01-30 21:02:02 +0600

    design a sample of a table

---
+++ table.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+	<body>
+		<table border="1" style="width:100%; border-collapse:collapse; text-align:center;">
+		
+		<tr>
+				<td colspan="4"; rowspan="3"; style="text-align:left; border-top-style:hidden; border-left-style:hidden; border-bottom-style:hidden">
+				<b>Attn: <u>Mr. HM. Mustafizur Rahman</u></b><br>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Vice President</td>
+				<td colspan="3" style="text-align:left; border-top-style:hidden; border-left-style:hidden; border-right-style:hidden">Invoice</td>
+				
+			</tr>
+			
+			<tr>
+				<td>No.</td>
+				<td colspan="2">SSL/16/02011</td>
+				
+			</tr>
+			
+			<tr>
+				<td>Day<br>01</td>
+				<td>Month<br>03</td>
+				<td>Year<br>2016</td>
+				
+			</tr> 
+			
+			<tr>
+				<td colspan="4" style="border-left-style:hidden; border-right-style:hidden"></td>
+				<td colspan="3" style="text-align:left; border-right-style:hidden">For payment or inquiries please quote abobe number & date</td>
+				
+				
+			</tr> 
+
+			<tr>
+				<td colspan="5" style="text-align:left">Customer's Refererance No.:Service Agreement for Mobile Based Service Solution<br>Dated: October 09 2013</td>
+				
+				<td colspan="2" rowspan="2"; style="text-align:center"><u>Billing Month</u> <br><br>February 16</td>
+			</tr>
+				
+			<tr>
+				<td rowspan="2"; colspan="2" style="text-align:left">Service Address<br> As Abobe</td>
+				<td colspan="3" style="text-align:center">Customer No.</td>
+				
+			</tr>
+				
+			<tr>
+				
+				<td colspan="3" style="text-align:center"><u>Billing Period</u><br><br>1-Feb 16
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				to &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;29-Feb 16</td>
+				<td colspan="2" ><u>Vat Reg No.</u><br><br>19061099850</td>
+				
+			</tr>
+				
+			<tr>
+				<td style="width:5%;">Sl</td>
+				<td style="width:25%">Descriptions of Goods/Services</td>
+				<td style="width:14%">Unit Qty</td>
+				<td style="width:14%">Unit Price<br>Taka</td>
+				<td style="width:14%">Amount<br> in Taka</td>
+				<td style="width:14%">Vat 4.5%</td>
+				<td style="width:14%">Net Price <br> Taka</td>
+			</tr>
+				
+			<tr>
+				<td></td>
+				<td style="text-align:left"><u><b>We change you for<b/></u></td>
+				<td colspan="5"></td>
+			
+			</tr>
+			
+			<tr>
+				<td>1</td>
+				<td style="text-align:left">push</td>
+				<td style="border-right-style:hidden">1</td>
+				<td style="border-right-style:hidden">4000</td>
+				<td style="border-right-style:hidden">4000</td>
+				<td style="border-right-style:hidden">180</td>
+				<td>4180</td>
+				
+			</tr>
+				
+			<tr>
+				<td></td>
+				<td>Total</td>
+				<td></td>
+				<td></td>
+				<td>4000</td>
+				<td>180</td>
+				<td>4180</td>
+			</tr>
+				
+			<tr>
+				<td></td>
+				<td style="text-align:left">Title of Charging</td>
+				<td colspan="5"></td>
+		
+			</tr>
+				
+			<tr>
+				<td>1</td>
+				<td style="text-align:left">push</td>
+				<td style="border-right-style:hidden">1</td>
+				<td style="border-right-style:hidden">4000</td>
+				<td style="border-right-style:hidden">4000</td>
+				<td style="border-right-style:hidden">180</td>
+				<td>4180</td>
+			</tr>
+				
+			<tr>
+				<td></td>
+				<td>Total</td>
+				<td></td>
+				<td></td>
+				<td>4000</td>
+				<td>180</td>
+				<td>4180</td>
+			</tr>
+				
+			<tr>
+				<td></td>
+				<td>Grand Total</td>
+				<td></td>
+				<td></td>
+				<td>4000</td>
+				<td>180</td>
+				<td>4180</td>
+			</tr>
+				
+		</table>
+	</body>
+</html>
